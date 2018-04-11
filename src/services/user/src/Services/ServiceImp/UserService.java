cf77112c618adac68208a7bd69b733ffe14666a2
newliar <1107325513@qq.com>
2018-04-11 19:40:13 +0800

    fix bugs of user module

--- src/services/user/src/Services/ServiceImp/UserService.java
+++ src/services/user/src/Services/ServiceImp/UserService.java
@@ -3,22 +3,26 @@ package Services.ServiceImp;
 import java.util.HashMap;
 import java.util.Map;
 
+import Dao.UserDao;
 import Services.IUserService;
-import Utils.sqlUtils;
 
 public class UserService implements IUserService {
 
 	//用户登陆认证
 	@Override
 	public String[][] userLogin(String userName, String pw) {
-		sqlUtils su = new sqlUtils();
-		su.connect();
-		Map<String, String> userAndPw = new HashMap<String, String>();
+		//变量声明
+		Map<String, String> userAndPw = null;
+		UserDao ud = null;
+		String infoArr[][] = null;
+		String demandArr[] = {"userID","userName","role","headPortrait"};
+		//构建Map
+		userAndPw = new HashMap<String, String>();
 		userAndPw.put("userName", userName);
 		userAndPw.put("pw", pw);
-		String demandArr[] = {"userID","userName","role","headPortrait"};
-		String infoArr[][] = su.select("user", userAndPw, demandArr, 1, 1);
-		su.close();
+		//调用UserDao层
+		ud = new UserDao();
+		infoArr = ud.queryUser(userAndPw, demandArr, 1, 1);
 		return infoArr;
 	}
 
--- src/services/user/src/Utils/sqlUtils.java
+++ src/services/user/src/Utils/sqlUtils.java
@@ -6,8 +6,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
