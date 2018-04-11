a100c72237892db5749d9cea6411d6a0d2b287c2
newliar <1107325513@qq.com>
2018-04-11 20:29:54 +0800

    add user pw sha256

--- src/services/user/src/Services/ServiceImp/UserService.java
+++ src/services/user/src/Services/ServiceImp/UserService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import Dao.UserDao;
 import Services.IUserService;
+import Utils.sha256Util;
 
 public class UserService implements IUserService {
 
@@ -15,11 +16,16 @@ public class UserService implements IUserService {
 		Map<String, String> userAndPw = null;
 		UserDao ud = null;
 		String infoArr[][] = null;
+		sha256Util sha256 = null;
 		String demandArr[] = {"userID","userName","role","headPortrait"};
+		String pw_sha256="";
+		//用户密码sha256加密
+		sha256 = new sha256Util();
+		pw_sha256 = sha256.getSHA256StrJava(pw);
 		//构建Map
 		userAndPw = new HashMap<String, String>();
 		userAndPw.put("userName", userName);
-		userAndPw.put("pw", pw);
+		userAndPw.put("pw", pw_sha256);
 		//调用UserDao层
 		ud = new UserDao();
 		infoArr = ud.queryUser(userAndPw, demandArr, 1, 1);
