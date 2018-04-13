82f4a248717cf013fa81e928b3fd060e7adb4ffd
ChenhuaFan <805901483@qq.com>
2018-04-14 04:14:04 +0800

    继续结局请求依赖链

--- src/apiGateway/index.js
+++ src/apiGateway/index.js
@@ -15,28 +15,56 @@ let services = JSON.parse(fs.readFileSync(__dirname + "/config/services.json"));
 let exApis = JSON.parse(fs.readFileSync(__dirname + "/config/exApis.json"));
 
 // 网关初始化
-const initServer = (app) => {
-    // 根据 exApis 动态配置路由。遍历api数组
+const initServer = () => {
+    // 根据 exApis 为每个api生成路由函数
     let apiArry = exApis.apis;
     let tempApi = "";
+    let a = {};
     for (let i = 0,len=apiArry.length; i < len; i++) {
         for(let url in apiArry[i]) {
             tempApi = (apiArry[i])[url];
-            if (tempApi.method == "get") {
-                // 在此注册get路由
-                app.use(route.get(url, (ctx, tempApi)=> builtBff()));
-            }
-            else {
-                // 在此注册post路由
-                app.use(route.post(url, (ctx, tempApi)=> builtBff()));
+            console.log(tempApi);
+            a[url] = {
+                method: tempApi.method,
+                bff: ctx => {
+                    const body = ctx.request.body;
+                    // 生成请求依赖链
+                    
+                }
             }
         }
+    }
+    //遍历注册所有路由
+    console.log(a);
+    for (let temU in a) {
+        if (a[temU].method == "post")
+            app.use(route.post(temU, a[temU].bff));
+        else
+            app.use(route.get(temU, a[temU].bff));
     }
 };
 
 const builtBff = (ctx, api) => {
+    console.log(api);
     let dependences = api.dependences;
+    let entrance = dependences.result;
+    // 申明层数（同一层并行，不同层穿行执行）
+    let depth = 0;
+    let relyConstruct = {};
+    // 从入口依赖数组出发，递归得到依赖结构
+    findParents(depth, entrance);
     
+    const findParents = (depth, node) => {
+        // 如果依赖数组为null，则说明已经到该层的头结点，退出.
+        if(node.rely == null)
+            console.log(depth+": "+ node);
+            return;
+        depth += 1;
+        for (let tempNode in node.rely) {
+            findParents(depth, dependences[tempNode]);
+        }
+    }
+
 }
 
 // 将当前配置写入JSON中
