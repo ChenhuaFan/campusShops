b9d3319ff0f9b1a81f3ea48587d49fcd47bbc773
ChenhuaFan <805901483@qq.com>
2018-04-13 20:42:44 +0800

    add get token method

--- src/services/token/index.js
+++ src/services/token/index.js
@@ -2,71 +2,58 @@ const Koa = require('koa');
 const app = new Koa();
 const route = require('koa-route');
 const koaBody = require('koa-body');
-const config = require('./config/default');
+const conf = require('./config/default');
 const base64url = require('base64url')
+const crypto = require('crypto');
 
 app.use(koaBody());
 
+const getCrypto = (type, key) => {
+    return crypto.createHmac(type, key);
+}
+
 const authGet = async ctx => {
-    const token = "";
-    //check payload
+    const tokenExp = conf.token;
     const post = ctx.request.body;
-    for(let key in config.payload) {
-        console.log(key + ": "+ (config.payload)[key]);
+    let token = "";
+    let header = conf.token.header;
+    let payload = conf.token.payload;
+    let secret = ""; 
+    //built payload
+    for(let key in payload) {
+        payload[key] = post[key];
     }
+    if(payload["role"]=="")
+    //prepare for builting secret
+    payload["iat"] = new Date().getTime();
+    payload["exp"] = parseInt(payload["iat"]) + tokenExp.limit.exp;
+    payload["nbf"] = parseInt(payload["exp"]);
+    //built secret
+    header = base64url(JSON.stringify(header));
+    payload = base64url(JSON.stringify(payload));
+    let hamc = getCrypto(tokenExp.header.alg, tokenExp.secret);
+    hamc.update(`${header}.${payload}`);
+    secret = hamc.digest('hex');
+    token = `${header}.${payload}.${secret}`;
+    //set response
+    ctx.response.status = 200;
+    ctx.response.type = 'json';
+    ctx.response.body = {
+        "token": token
+    };
 };
 
 const authUpdate = ctx => {
     //do update a old token and return a new token if former token is legal
+    ctx.response.status = 200;
+    ctx.response.type = 'json';
+    ctx.response.body = {
+        "status": true,
+        "info": "ab"
+    }
 }
 
-/*
-const example = ctx => {
-//do signin and return a token if user info is correct.
-    const body = ctx.request.body;
-    //request other apis and get return values
-    let userReq = body => {
-        return superagent
-        .post('http://localhost:3000/user/auth')
-        .type('application/json')
-        .set('Accept', 'application/json')
-        .send(body)
-        .then((res, err) => {
-            return JSON.parse(res.res.text);
-        });
-    }
-    //async get return values and set response
-    let userRes = await userReq(body);
-    if(userRes.status != false) {
-        //generate token
-        let now = time.now();
-        let payload = {
-            "name": "samchevia",
-            "uuid": 1,
-            "role": "admin",
-            "iat": now,
-            "exp": config.jwt.payload.exp,
-            "nbf": now + config.jwt.payload.exp
-        };
-        let token = jwt.create(config.jwt.head, payload, config.jwt.secret);
-        //set response
-        ctx.response.status = 200;
-        ctx.response.type = 'json';
-        ctx.response.body = {
-            "status": true,
-            "info": token
-        }
-    } else {
-        ctx.response.status = 403;
-        ctx.response.type = 'json';
-        ctx.response.body = {
-            "status": false,
-            "info": userRes.info
-        }
-    }
-}
-*/
 app.use(route.post('/token/get', authGet));
 app.use(route.post('/token/update', authUpdate));
 
-app.listen(3000);
+app.listen(3030);
