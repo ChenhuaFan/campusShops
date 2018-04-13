a30325f7972b1686e1e4e12a121d753120c09c48
ChenhuaFan <805901483@qq.com>
2018-04-14 02:33:38 +0800

    动态注册路由

--- src/apiGateway/index.js
+++ src/apiGateway/index.js
@@ -4,32 +4,47 @@ const route = require('koa-route');
 const koaBody = require('koa-body');
 const radis = require('radis');
 const request = require('superagent');
+let conf = require('./config/default');
+const fs = require('fs');
+const path=require('path');
 
-// 服务器初始化
 
+// 读取 services.json & exApis.json
+// 使用 path.join 构建配置文件路径
+let services = JSON.parse(fs.readFileSync(__dirname + "/config/services.json"));
+let exApis = JSON.parse(fs.readFileSync(__dirname + "/config/exApis.json"));
 
-// 日志记录中间件
-const logger = (ctx, next) => {
-    // before
-    next();
-    // after
+// 网关初始化
+const initServer = (app) => {
+    // 根据 exApis 动态配置路由。遍历api数组
+    let apiArry = exApis.apis;
+    let tempApi = "";
+    for (let i = 0,len=apiArry.length; i < len; i++) {
+        for(let url in apiArry[i]) {
+            tempApi = (apiArry[i])[url];
+            if (tempApi.method == "get") {
+                // 在此注册get路由
+                app.use(route.get(url, (ctx, tempApi)=> builtBff()));
+            }
+            else {
+                // 在此注册post路由
+                app.use(route.post(url, (ctx, tempApi)=> builtBff()));
+            }
+        }
+    }
 };
 
-// 鉴权记录中间件
-const auth = (ctx, next) => {
-    // before
-    next();
-    // after
-};
+const builtBff = (ctx, api) => {
+    let dependences = api.dependences;
+    
+}
 
-// api跟踪中间件
-const trace = (ctx, next) => {
-    // before
-    next();
-    // after
-};
+// 将当前配置写入JSON中
+const saveToJson = () => {
 
-// 错误处理中间件
+}
+
+// 1. 错误处理中间件
 const handler = async (ctx, next) => {
     try {
       await next();
@@ -43,6 +58,27 @@ const handler = async (ctx, next) => {
     }
 };
 
+// 2. api跟踪中间件
+const trace = (ctx, next) => {
+    // before
+    next();
+    // after
+};
+
+// 3. 日志记录中间件
+const logger = (ctx, next) => {
+    // before
+    next();
+    // after
+};
+
+// 4. 鉴权记录中间件
+const auth = (ctx, next) => {
+    // before
+    next();
+    // after
+};
+
 // web界面监控
 const web = ctx => {
     ctx.response.status = 200;
@@ -54,6 +90,8 @@ app.on('error', (err) => {
     console.log('info: ' + err.message);
     console.log(err);
 });
+
+initServer();
 
 // 注册中间件
 // 注册表单表单数据处理
