a0e6374f0e423049d196e98706ba80b52a43ccea
ChenhuaFan <805901483@qq.com>
2018-04-25 19:33:03 +0800

    fix port

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -12,5 +12,17 @@ sudo rm -rf /var/www/bff/
 sudo mv -i /tmp/campusShops/src/services/ /var/www/
 sudo mv -i /tmp/campusShops/src/webServer/ /var/www/
 sudo mv -i /tmp/campusShops/src/bff/ /var/www/
+sudo mv -i /tmp/campusShops/dist/pm2.json /var/www/
 
-# npm install
+# npm install & redeploy pm2
+cd /var/www/webServer/
+sudo npm install
+
+cd /var/www/services/token/
+sudo npm install
+
+cd /var/www/bff/
+sudo npm install
+
+cd /var/www/
+pm2 start pm2.json
