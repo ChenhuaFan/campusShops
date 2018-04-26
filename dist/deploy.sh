b0f3db013f48855ae64d9d458cbfecfe9b9540b6
ChenhuaFan <805901483@qq.com>
2018-04-26 13:18:13 +0800

    add order part & update deploy script

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -15,21 +15,40 @@ sudo mv -i /tmp/campusShops/src/bff/ /var/www/
 sudo mv -i /tmp/campusShops/dist/pm2.json /var/www/
 
 # npm install & redeploy pm2
+# services 3000
 cd /var/www/webServer/
 sudo npm install
-
+# 5000
 cd /var/www/services/token/
 sudo npm install
 
-cd /var/www/bff/
+# bff
+# 4001
+cd /var/www/bff/admin
+sudo npm install
+# 4000
+cd /var/www/bff/auth
+sudo npm install
+# 4002
+cd /var/www/bff/product
+sudo npm install
+# 4003
+cd /var/www/bff/shop
+sudo npm install
+# 4004
+cd /var/www/bff/user
+sudo npm install
+# 4005
+cd /var/www/bff/order
 sudo npm install
 
+# pm2
 cd /var/www/
 pm2 start pm2.json
 pm2 save
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u rjxz --hp /home/rjxz
 
-# clear tem files
+# clear tmp files
 cd /tmp/
 sudo rm -rf campusShops/
 sudo rm -rf shop.tar
